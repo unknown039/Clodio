ed290fbd51beef6750613c9d13166e937908cdf0
unknown <unknown@unknown.unknown>
2026-09-16 20:09:30 -0500

    Changed text font, weight, and alignment

--- app/src/main/java/app/clodio/screens/Home.kt
+++ app/src/main/java/app/clodio/screens/Home.kt
@@ -1,6 +1,7 @@
 package app.clodio.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -10,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.clodio.model.Alarm
 import app.clodio.model.Alarm.Companion.getRandomAlarm
 
@@ -54,7 +57,15 @@ fun AlarmBox(alarm : Alarm) {
                 Color.LightGray,
                 shape = RoundedCornerShape(10.dp)
             )
+            .clickable(){}
     ) {
-
+        Text(
+            text = alarm.time.toString(),
+            fontSize = 20.sp,
+            fontWeight = FontWeight(100),
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 10.dp)
+        )
     }
 }
